6cd957eaa6a50d6ebbee9b5202fc29e36088ad8b
teten777 <teten.nugraha18@gmail.com>
2019-08-15 11:42:03 +0700

    update

---
+++ src/main/java/com/starter/kit/demostarterkit/DemoStarterKitApplication.java
@@ -0,0 +1,13 @@
+package com.starter.kit.demostarterkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStarterKitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoStarterKitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/starter/kit/demostarterkit/DemoStarterKitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.starter.kit.demostarterkit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoStarterKitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
